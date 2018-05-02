c69c867f76cc12c6f630af0787994877a0741b8a
Oleg Matvejev <omatveje@gmail.com>
2018-05-01 22:56:51 -0700

    Quick logger middleware displaying actions received

---
+++ README.md
@@ -0,0 +1,7 @@
+# Quick Node/Express/Postgres bootstrap
+
+How to run it:
+1. Install nodemon ```npm install -g nodemon```
+2. Install docker and docker-compose
+3. Run ```docker-compose up```
+4. Run ```nodemon```
--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const mountRoutes = require('./routes');
+const middleware = require('./middleware');
 
 const app = express();
 
+middleware(app);
 mountRoutes(app);
 
 app.listen(3000, () => console.log('Starting App on port 3000!'));
